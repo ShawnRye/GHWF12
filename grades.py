ddf910b284339effb170a78bde2d9b66a610bdce
Shawn Ryer <Ryers@moravian.edu>
2022-02-10 08:41:28 -0500

    Add function comment

--- grades.py
+++ grades.py
@@ -1,5 +1,8 @@
 
 def compute_hw_average(grades):
+    """
+    Compute the averagr grade for the list of assignment grades.
+    """
     if len(grades) == 0:
         return 0
     return grades[0]
